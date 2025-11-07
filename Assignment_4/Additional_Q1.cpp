2a7c6f51ea8157a2aff5f3fedef25a0aa9528013
Yashvardhan-Chaudhry <ychaudhry_be24@thapar.edu>
2025-11-07 12:19:46 +0530

    Create Additional_Q1.cpp

---
+++ Assignment_4/Additional_Q1.cpp
@@ -0,0 +1,38 @@
+/*Given a function n, write a function that generates and prints all binary numbers with decimal values
+from 1 to n.*/ 
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+vector<string> generateBinary(int n) {
+    vector<string> result;
+    if (n <= 0) return result;
+
+    queue<string> q;
+    q.push("1");
+
+    for (int i = 1; i <= n; i++) {
+        string s = q.front();
+        q.pop();
+        result.push_back(s);
+        q.push(s + "0");
+        q.push(s + "1");
+    }
+    return result;
+}
+
+void printBinaryNumbers(vector<string>& nums) {
+    for (string s : nums)
+        cout << s << " ";
+    cout << "\n";
+}
+
+int main() {
+    int n=5;
+    
+    vector<string> binaries = generateBinary(n);
+    printBinaryNumbers(binaries);
+    return 0;
+}
